6010e591a90b088d8815e5b20d3a8cf51fc8c687
sato-ayaka-732 <100703ayaka@gmail.com>
2026-04-23 17:06:17 +0900

    Hashrouterに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./iFlames/pages/default/Home";
 import Count from "./iFlames/pages/1/Count";
 import StringEdit from "./iFlames/pages/1/StringEdit";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/count" element={<Count />} />
         <Route path="/string-edit" element={<StringEdit />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
